721f75728b5db76ec1962fa60cdae4d01351979b
Tomas Michael <tomasmichael995@gmail.com>
2018-11-30 00:59:21 +0200

    PingPongBackendApp prints welcome

--- PingPongBackendApp.java
+++ PingPongBackendApp.java
@@ -2,6 +2,14 @@ package gr.skemelio.ping_pong;
 
 public class PingPongBackendApp{
   public static void main(String[] args) {
+    printWelcome();
+    MultiServer server = new MultiServer();
+  }
 
+  private static void printWelcome(){
+    System.out.println("*******************************");
+    System.out.println("********* Ping/Pong ***********");
+    System.out.println("********* Welcome! ************");
+    System.out.println("*******************************");
   }
 }
