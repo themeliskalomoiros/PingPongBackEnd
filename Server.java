06a20299dce16a0888ef2fb60173187d45d165b1
Tomas Michael <tomasmichael995@gmail.com>
2018-11-29 23:54:01 +0200

    Server accepts requests from clients

--- Server.java
+++ Server.java
@@ -2,6 +2,7 @@ package gr.skemelio.ping_pong;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.IOException;
+import java.net.SocketTimeoutException;
 
 // A multiclient server which pings and gets ponged!
 
@@ -19,6 +20,11 @@ public class Server extends Thread{
   public void run(){
     try {
       serverSocket = new ServerSocket(0);
+      while (true) {
+        Socket clientSocket = serverSocket.accept();
+      }
+    } catch (SocketTimeoutException e) {
+      
     } catch(IOException e) {
 
     }
