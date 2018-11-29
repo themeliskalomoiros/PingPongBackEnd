f82ece72b631e3064ea04ebaee03e7599ae72515
Tomas Michael <tomasmichael995@gmail.com>
2018-11-29 23:47:36 +0200

    Created OnServerUpListener for Server

--- Server.java
+++ Server.java
@@ -1,10 +1,26 @@
 package gr.skemelio.ping_pong;
 import java.net.ServerSocket;
 import java.net.Socket;
-/*
-  Server pings, clients pong.
-*/
+import java.io.IOException;
+
+// A multiclient server which pings and gets ponged!
 
 public class Server extends Thread{
 
+  public interface OnServerUpListener{
+    void onServerUp(String host, int port);
+    void onServerUpFailure(IOException e);
+  }
+
+  private OnServerUpListener onServerUpListener;
+  private ServerSocket serverSocket;
+
+  @Override
+  public void run(){
+
+  }
+
+  public void setOnServerUpListener(OnServerUpListener listener){
+    onServerUpListener = listener;
+  }
 }
