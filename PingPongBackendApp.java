66cc6a65834c674b2865a603af7e587b64585805
Tomas Michael <tomasmichael995@gmail.com>
2018-11-30 01:05:21 +0200

    PingPongBackendApp starts the server at main()

--- PingPongBackendApp.java
+++ PingPongBackendApp.java
@@ -1,12 +1,29 @@
 package gr.skemelio.ping_pong;
 
-public class PingPongBackendApp{
+import java.io.IOException;
+
+public class PingPongBackendApp implements MultiServer.OnServerBoundListener{
+
   public static void main(String[] args) {
-    printWelcome();
+    PingPongBackendApp application = new PingPongBackendApp();
+    application.printWelcome();
+
     MultiServer server = new MultiServer();
+    server.setOnServerBoundListener(application);
+    server.start();
   }
 
-  private static void printWelcome(){
+  @Override
+  public void onServerBound(String host, int port){
+    System.out.printf("A server bound succesfully!\nHost:\t%s\nPort:%d\t",host,port);
+  }
+
+  @Override
+  public void onServerBoundFailure(IOException e){
+    System.out.println(e.getMessage());
+  }
+
+  private void printWelcome(){
     System.out.println("*******************************");
     System.out.println("********* Ping/Pong ***********");
     System.out.println("********* Welcome! ************");
